e7ad6a153e81a719f1d67baf6367bf34fa15bed2
ÉlianePelletier <2344365@cegeplimoilou.ca>
2025-10-23 10:54:59 -0400

    ajout des prototypes dans le fichier .h (fonctionsChansons)

--- DanseEnLigne/src/fonctionsChansons.h
+++ DanseEnLigne/src/fonctionsChansons.h
@@ -10,12 +10,47 @@ void brasGBas();
 void brasDBas();
 
 //chanson 2
+void delRouge();
+void delJaune();
+void delVerte();
+void delBleue();
+void delRBrasDevant();
+void delJBrasHaut();
+void delVBrasDevant();
+void delBBrasBas();
+void _360HoraireDelAleatoiresBrasHaut();
+void delREtJAlternBrasBas();
+void delJEtVAltern();
+void delBEtRAltern();
+void _360AntiHoraireDelAleatoiresBrasAvant();
 
 //chanson 3
+void position1BrasBas();
+void position2();
+void position3();
+void position4();
+void position1();
 
 //chanson 4
+void changementPlaceMoins1();
+void position1DelR();
+void position2DelJ();
+void position3DelV();
+void position4DelB();
+void position0ToutesDel();
+void _720HoraireDelAleatoires();
+void changementPlacePlus1();
+void position0DelAleatoires();
 
 //chanson 5
+void position1DelRBrasDevant();
+void position2DelJBrasHaut();
+void position3DelVBrasBas();
+void position4DelBBrasDevant();
+void position1BrasHaut();
+void position2BrasGHautBrasDDevant();
+void position3BrasDevant();
+void position4BrasGDevantBrasDHaut();
 
 //chanson 6
 
