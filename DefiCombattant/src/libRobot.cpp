ceff70b02c4649c9c87736c072a8164cff34d91a
BenjaminRD <royb5170@usherbrooke.ca>
2025-10-27 14:15:59 -0400

    Réimplantations des codes pour la danse et le mur
    Fonctions avance et tourne ajoutées en librairie

--- DefiCombattant/src/libRobot.cpp
+++ DefiCombattant/src/libRobot.cpp
@@ -13,6 +13,68 @@ int   PULSE_180=3864;
 int   PULSE_270=5796;
 float V=0.35;
 
+/*
+********************************************************************************
+                         FONCTIONS DE ROMAN + BENJAMIN
+
+********************************************************************************
+
+*/
+
+
+float vitesse = 0.4; // vitesse par défaut
+
+// conversion cm -> ticks encodeur
+int cmToTicks(float cm) {
+  return cm * 13581; // ajuster selon roue
+}
+
+// conversion angle -> ticks encodeur
+int angleToTicks(float angle) {
+  float distance = (angle / 360.0) * 0.195 * PI; // circonférence roue = 0.195 m
+  return distance * 13581;
+}
+
+void avance(float cm) {
+  int distanceTicks = cmToTicks(cm);
+  int pv_left = ENCODER_Read(LEFT);
+  int pv_right = ENCODER_Read(RIGHT);
+
+  MOTOR_SetSpeed(LEFT, vitesse);
+  MOTOR_SetSpeed(RIGHT, vitesse);
+
+  while ((ENCODER_Read(LEFT) - pv_left < distanceTicks) && 
+         (ENCODER_Read(RIGHT) - pv_right < distanceTicks)) {
+    delay(10);
+  }
+
+  MOTOR_SetSpeed(LEFT, 0);
+  MOTOR_SetSpeed(RIGHT, 0);
+}
+
+void tourne(float angle, int sens) {
+  int distanceTicks = angleToTicks(angle);
+  int pv_left = ENCODER_Read(LEFT);
+  int pv_right = ENCODER_Read(RIGHT);
+
+  if (sens == 1) { // droite
+    MOTOR_SetSpeed(LEFT, vitesse);
+    MOTOR_SetSpeed(RIGHT, -vitesse);
+    while ((ENCODER_Read(LEFT) - pv_left < distanceTicks) &&
+           (pv_right - ENCODER_Read(RIGHT) < distanceTicks)) {
+      delay(10);
+           }}}
+
+
+
+
+
+/*
+******************************************************************
+                         FONCTIONS DE TRISTAN
+
+******************************************************************
+*/
 void TournerDroite90_PROVISOIRE()
 {
     
@@ -23,7 +85,7 @@ void TournerDroite90_PROVISOIRE()
     MOTOR_SetSpeed(0, V);   
     MOTOR_SetSpeed(1, -V);  
 
-    while (fabs(ENCODER_Read(0)) < PULSE_90 && fabs(ENCODER_Read(1)) < PULSE_90)
+    while (fabs(ENCODER_Read(0)) < PULSE_90 && fabs(ENCODER_Read(1)) > (-1)*PULSE_90)
     {
        
     }
@@ -218,3 +280,15 @@ void quille(void)
 
 
 
+  
+
+
+void ajusteRotation(int angleCible, int tolerance){
+    // Convertir les angles en pulses de roue
+
+    // Calcul du nombre de pulses nécessaires pour faire un 360
+
+    float pulses360 = ((20/2)*PI);
+    int pulsesTarget = (pulses360 * angleCible) / 360;  //Voici le nombre de pulses nécessaires pour l'angle cible
+    
+}
--- DefiCombattant/src/main.cpp
+++ DefiCombattant/src/main.cpp
@@ -16,9 +16,40 @@ void loop()
   //quand sort du while, check quelle couleur (ou absence/couleur plancher?) et appel bonne méthode
   
 }
-/*
+
 //fonction de départ lorsqu'il y a le carton bleu (petite danse)
 void departDanseBleu(){
+    //Départ
+  tourne (45, 1);
+  delay(50);
+ 
+  avance (30);
+  delay(50);
+ 
+  tourne (90, 0);
+  delay(50);
+  
+  avance (30);
+  delay(50);
+ 
+  tourne (90, 0);
+  delay(50);
+  
+  avance (30);
+  delay(50);
+  
+  tourne (90, 0);
+  delay(50);
+
+  avance (30);
+  
+  delay(200);
+  //fin 
+
+  // Retour sur la ligne 
+  tourne (135, 0);
+  
+
 
 }
 
@@ -29,6 +60,34 @@ void departQuilleRose(){
 
 //fonction de départ pour lorsqu'il y a le carton jaune (contourner mur)
 void departMurJaune(){
+     // Avance 50 cm
+  avance(10);
+
+  // Tourne 90° à droite
+  tourne(90, 1);
+
+  // Avance 25 cm
+  avance(25);
+
+  // Tourne 90° à gauche
+  tourne(90, 0);
+
+  // Avance 25 cm pour contourner le mur
+  avance(10);
+
+   // Tourne 90° à gauche
+  tourne(90, 0);
+
+  // Avance 25 cm pour contourner le mur
+  avance(10);
+
+    // Tourne 90° à droite
+  tourne(90, 1);
+}
+/*
+//fonction de départ lorsqu'il y a le carton vert (quand il n'y a plus de ligne)
+void departRetrouverLigneVert(){
+
     
 }
 
