8ef83b80a097ae9c3a8c2ec6863f3f1e2981dcd6
Alex <bioviruss1234@gmail.com>
2025-10-28 13:50:29 -0400

    Test pour chanson 2

--- DanseEnLigne/src/chanson2.cpp
+++ DanseEnLigne/src/chanson2.cpp
@@ -4,38 +4,55 @@
 
 void delRouge()
 {
+    allumeFermeAvecDelay(3500,rougePin);
 }
 
 void delJaune()
 {
+    allumeFermeAvecDelay(3500,jaunePin);
 }
 
 void delVerte()
 {
+    allumeFermeAvecDelay(3500,vertPin);
 }
 
 void delBleue()
 {
+    allumeFermeAvecDelay(3500,bleuPin);
 }
 
 void delRBrasDevant()
 {
+    allumeFermeAvecDelay(3500,rougePin);
+    bougerBrasDroit(angleBrasDevant);
+    bougerBrasGauche(angleBrasDevant);
 }
 
 void delJBrasHaut()
 {
+    allumeFermeAvecDelay(3500,jaunePin);
+    bougerBrasDroit(angleBrasHaut);
+    bougerBrasGauche(angleBrasHaut);
 }
 
 void delVBrasDevant()
 {
+    allumeFermeAvecDelay(3500,vertPin);
+    bougerBrasDroit(angleBrasDevant);
+    bougerBrasGauche(angleBrasDevant);
 }
 
 void delBBrasBas()
 {
+    allumeFermeAvecDelay(3500,bleuPin);
+    bougerBrasDroit(angleBrasBas);
+    bougerBrasGauche(angleBrasBas);
 }
 
 void _360HoraireDelAleatoiresBrasHaut()
 {
+    
 }
 
 void delREtJAlternBrasBas()
