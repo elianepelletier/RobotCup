fe0287d38d9021803e69aabae91348284a4e3698
Alex <bioviruss1234@gmail.com>
2025-10-29 13:40:43 -0400

    PLAN MACHIAVÉLIQUE CHANSON 6

--- DanseEnLigne/src/chanson6.cpp
+++ DanseEnLigne/src/chanson6.cpp
@@ -1,20 +1,27 @@
 //va contenir toutes les fonctions décrivant les mouvements qui se produiront pendant la chanson #6
 #include <mouvementsBase.h>
 #include "fonctionsChansons.h"
+#include <libRobot.h>
 
 //le reste des fonctions existent déjà dans la chanson 5
-void position1BrasHautDelAleatoiresFin()
-{
-}
+//void position1BrasHautDelAleatoiresFin()
+//{
+//}
+//
+//void position1BrasHaut360()
+//{
+//}
+//
+//void position3BrasDevant360()
+//{
+//}
+//
+//void faire1080()
+//{
+//}
 
-void position1BrasHaut360()
+//PLAN MACHIAVÉLIQUE DU DR.DOOFENSCHMIRTZ
+void position0()
 {
-}
-
-void position3BrasDevant360()
-{
-}
-
-void faire1080()
-{
+    tourne(7200, true);
 }
